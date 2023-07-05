1f7d79c2850c1e7d081f5a8e3325a49ca7387517
saji2000 <sajaddaneshmand79@gmail.com>
2023-07-04 21:50:15 -0600

    conditional statements

--- react-app/src/Message.tsx
+++ react-app/src/Message.tsx
@@ -1,6 +1,9 @@
 function Message(){
-    const name = 'Sajad';
-    return <h1>Hello {name}</h1>;
+    const name = '';
+    if (name) {
+        return <h1>Hello {name}</h1>;
+    }
+    return <h1>Hello World</h1>;
 }
 
 export default Message;
