771f4c44625ba2093d98cdb3c6cd58550050f894
saji2000 <sajaddaneshmand79@gmail.com>
2023-07-06 22:15:00 -0600

    arrow functions, praise God

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,12 +1,21 @@
 function ListGroup() {
-  const items = ["New York", "Tehran", "Shiraz", "Toronto"];
+  let items = ["New York", "Tehran", "Shiraz", "Toronto"];
+
+  items = [];
+
+  const getMessage = () => {
+    return items.length === 0 && <p>No item found</p>;
+  };
 
   return (
     <>
       <h1>My List</h1>
+      {getMessage()}
       <ul className="list-group">
         {items.map((item) => (
-          <li className="list-group-item">{item}</li>
+          <li className="list-group-item" key={item}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
